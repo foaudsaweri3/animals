4326166ba209cfbd766d711c95dd60ae5f408360
foaudsaweri3 <foaudsaweri3@gmail.com>
2024-11-25 22:03:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Add your JavaScript code here
-console.log("Welcome to the Animal Website!");
+// Add smooth scrolling to navigation links
+document.querySelectorAll('nav ul li a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
